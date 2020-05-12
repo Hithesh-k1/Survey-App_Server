a032b83c21e7098f403b74cb7bde3a1e3e0efb7b
unknown <hitheshk27@gmail.com>
2020-05-12 10:11:30 +0530

    Google Oauth

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express");
+require("./services/passport");
+
+
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send({hi:'there'})
-})
+const PORT = process.env.PORT || 5000;
 
-//initial commit
-const PORT =process.env.PORT || 5000
-
-app.listen(PORT) 
+app.listen(PORT);

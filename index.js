8471d4a05c88ec9d9483cc5484e5463b3f35ee72
unknown <hitheshk27@gmail.com>
2020-05-18 11:28:39 +0530

    cookie-session

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+const cookieSession = require("cookie-session")
+const passport = require('passport')
 require("./models/User");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 const app = express();
+app.use(
+    cookieSession({
+        maxAge:30*24*60*60*1000,
+        keys:[keys.cookieKey]
+    })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
 const authRoutes = require("./routes/authRoutes");
 authRoutes(app);
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,4 +13,13 @@ module.exports = (app) => {
    * After Google grants permission
    */
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 };
